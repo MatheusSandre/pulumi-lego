22ee070f8651c07c0e8db9cb45fcdb3244e0ad79
Matheus Sandre <matheus_sandre@hotmail.com>
2025-08-18 15:14:54 -0300

    Update block_alb_ecs_service.py

--- block_alb_ecs_service.py
+++ block_alb_ecs_service.py
@@ -16,7 +16,7 @@ class Service:
     def create_service(cluster, prefix, project_name, service_name, cpu, memory, environment,
                        vpc, policies_roles, alb, app_port, container_definitions_json, volume,
                        service_discovery, ignore_container_definitions_changes,
-                       circuit_breaker_enabled):
+                       circuit_breaker_enabled, public_ip = False):
 
         tags = {
             "Environment": environment,
@@ -162,7 +162,7 @@ class Service:
                 }
             ],
             network_configuration={
-                "assignPublicIp": False,
+                "assignPublicIp": public_ip,
                 "securityGroups": [ecs_sg],
                 "subnets": vpc["privateSubnetIDs"]
             },
