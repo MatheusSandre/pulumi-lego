87f159f039871b8c43f49b9aa524ac5b7a9d52ca
MatheusSandre <matheus_sandre@hotmail.com>
2025-08-29 14:30:08 -0300

    change efs

--- block_worker_ecs_service.py
+++ block_worker_ecs_service.py
@@ -32,38 +32,21 @@ class Worker:
                 subnet_id=vpc["privateSubnetIDs"][0]
             )
 
-            efs_ap = EFS.create_accesspoint(
-                name=f"{resource_name}-efs-ap",
-                file_system_id=volume_efs.id,
-                posix_user={
-                    "uid": user_id,
-                    "gid": user_id,
-                },
-                root_directory={
-                    "path": "/",
-                    "creation_info": {
-                        "owner_uid": user_id,
-                        "owner_gid": user_id,
-                        "permissions": "750",
-                    }
-                },
-                tags=tags
-            )
-
             volumes = [
                 {
                     "name": f"{resource_name}-efs",
+                    "host": None,
+                    "dockerVolumeConfiguration": None,
                     "efsVolumeConfiguration": {
+                        "transitEncryptionPort": None,
                         "fileSystemId": volume_efs.id,
                         "authorizationConfig": {
-                            "accessPointId": efs_ap.id,
-                            "iam": "DISABLED"
-                        },
-                        "rootDirectory": "/"
-                    }
+                            "iam": "DISABLED",
+                            "accessPointId": None},
+                        "transitEncryption": "DISABLED",
+                        "rootDirectory": "/"}
                 }
             ]
-
         else:
             volumes = []
 
